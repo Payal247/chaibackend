2eb76d0221b6595beaeab8f37cdcc22941cb383e
Payal247 <125474816+Payal247@users.noreply.github.com>
2024-08-30 14:55:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 app.get('/', (req, res) => {
-    res.send("Rishabh")
+    res.send("Harshit")
 })
 app.get('/twitter', (req, res) => {
-    res.send('Lovedotcom')
+    res.send('Harshitdotcom')
 })
 app.get('/login', (req, res) => {
     res.send('<h1> please login chai aur code </h1>')
